9835fef2ac65d9f935d26d6c5b65bb72c0bf50d9
komprinat <asuhachigusa81@gmail.com>
2026-08-22 11:48:30 +0700

    Initial commit

--- server/config.py
+++ server/config.py
@@ -1,7 +1,7 @@
-MQTT_BROKER = "127.0.0.1"
-MQTT_PORT = 1883
+MQTT_BROKER = "127.0.0.1" #ip mqtt broker default 127.0.0.1
+MQTT_PORT = 1883 #port mqtt default 1883
 
-MQTT_TOPIC_POST = "esp32/perintah"
+MQTT_TOPIC_POST = "esp32/perintah" 
 MQTT_TOPIC_RECEIVE = "esp32/data_sensor"
 
-Gemini_key = "API KEY Gemini"
+Gemini_key = "API KEY Gemini" # Gemini API Key form google studio AI
